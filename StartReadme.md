7cb81bfd50b1f5a9785c86ce1c557167ff93f9bc
PrathamDwivedi27 <pratham.gungun12345@gmail.com>
2024-05-23 17:50:52 +0530

    basic body-parser and dotenv is added

---
+++ StartReadme.md
@@ -0,0 +1,5 @@
+1) sabse pehle npm init, express, nodemon, body-parser ye teen basic depencdencies
+2) express mein server start hum function mein karenge 
+3) humne dotenv use karna seekha hai  
+4) git mein push kar dena ek baar pehle hi
+5) 
---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const {PORT}=require("./config/serverconfig");      //curly braces is important in importing
+
+//we will make an object 
+
+const setupAndStartServer= async()=>{
+
+    const app=express();
+    // const PORT=3000;        //we don't want other people to know our port that's whywe use environment variables. one such package is dotenv
+
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.json());
+
+
+
+    app.listen(PORT,function process(){
+        console.log(`Server started at ${PORT}`);
+    })
+    // console.log(process.env);
+}
+
+setupAndStartServer();
